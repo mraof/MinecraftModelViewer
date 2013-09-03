45dc157ab35785aaa1fe68b2d308f8f72bbaa52a
mraof <mraoflink@verizon.net>
2013-09-02 22:28:32 -0400

    Made createModelFromSource

--- main.cpp
+++ main.cpp
@@ -45,15 +45,66 @@ int main()
     OS::init();
     Graphics::init();
     resizeGL(640, 480);
-    std::string modelClass = "ModelImp";
+    std::string modelClass = "ModelNull";
     Model model(modelClass);
+    //temporary, only for testing
+    std::string inputSrc = "\
+public class ModelImp extends ModelBase\n\
+{\n\
+	//fields\n\
+	ModelRenderer Head;\n\
+	ModelRenderer Body;\n\
+	ModelRenderer Armright;\n\
+	ModelRenderer Armleft;\n\
+	ModelRenderer Legleft;\n\
+	ModelRenderer Legright;\n\
+\n\
+	public ModelImp()\n\
+	{\n\
+		textureWidth = 32;\n\
+		textureHeight = 32;\n\
+\n\
+		Head = new ModelRenderer(this, 0, 0);\n\
+		Head.addBox(-3F, -3F, -5F, 5, 5, 5);\n\
+		Head.setRotationPoint(0F, 15F, 0F);\n\
+		Head.mirror = true;\n\
+		setRotation(Head, 0F, 0F, 0F); \n\
+		Body = new ModelRenderer(this, 0, 10);\n\
+		Body.addBox(-3F, -4F, -2F, 5, 6, 4);\n\
+		Body.setRotationPoint(0F, 19F, 0F);\n\
+		Body.mirror = true;\n\
+		setRotation(Body, 0F, 0F, 0F);\n\
+		Armright = new ModelRenderer(this, 0, 20);\n\
+		Armright.addBox(-1F, 0F, -1F, 1, 5, 1);\n\
+		Armright.setRotationPoint(-3F, 16F, 0F);\n\
+		Armright.mirror = true;\n\
+		setRotation(Armright, 0F, 0.0371786F, 0.0371786F); \n\
+		Armleft = new ModelRenderer(this, 0, 20);\n\
+		Armleft.addBox(0F, 0F, -1F, 1, 5, 1);\n\
+		Armleft.setRotationPoint(2F, 16F, 0F);\n\
+		Armleft.mirror = true;\n\
+		setRotation(Armleft, 0F, 0F, 0F);\n\
+		Legleft = new ModelRenderer(this, 4, 20);\n\
+		Legleft.addBox(-1F, 0F, 0F, 1, 3, 1);\n\
+		Legleft.setRotationPoint(-1F, 21F, 0F);\n\
+		Legleft.mirror = true;\n\
+		setRotation(Legleft, 0F, 0F, 0F);\n\
+		Legright = new ModelRenderer(this, 4, 20);\n\
+		Legright.addBox(0F, 0F, 0F, 1, 3, 1);\n\
+		Legright.setRotationPoint(0F, 21F, 0F);\n\
+		Legright.mirror = true;\n\
+		setRotation(Legright, 0F, 0F, 0F);\n\
+	}\n\
+}\n\
+";
+    createModelFromSource(&model, inputSrc);
     std::string partName = "Head";
-    model.addPart(partName);
-    model.parts[partName]->textureOffsetX = 0;
-    model.parts[partName]->textureOffsetY = 0;
-    model.parts[partName]->setRotationPoint(1.0f, 0.5f, 0.25f);
-    model.parts[partName]->addBox(-3, -3, 0, 10, 1, 1);
-    model.parts[partName]->addBox(-3, 0, 4, 10, 1, 1);
+//    model.addPart(partName);
+//    model.parts[partName]->textureOffsetX = 0;
+//    model.parts[partName]->textureOffsetY = 0;
+//    model.parts[partName]->setRotationPoint(1.0f, 0.5f, 0.25f);
+//    model.parts[partName]->addBox(-3, -3, 0, 10, 1, 1);
+//    model.parts[partName]->addBox(-3, 0, 4, 10, 1, 1);
     std::string src = "";
     createSourceFromModel(&model, src);
     std::cout << src;
--- parser.cpp
+++ parser.cpp
@@ -1,6 +1,19 @@
 #include "parser.h"
 #include <sstream>
+#include <iostream>
 
+size_t endOfNum(std::string &line, size_t start)
+{
+    size_t varEnd = line.find_first_of("Ff,)", start);
+//    if(varEnd == std::string::npos)
+//    {
+//        varEnd = line.find("f", start);
+//        if(varEnd == std::string::npos)
+//            varEnd = line.find(",", start);
+//    }
+    return varEnd;
+}
+//far from perfect, but it should work usually
 void createModelFromSource(Model* model, std::string &src)
 {
     std::istringstream srcStream;
@@ -8,7 +21,71 @@ void createModelFromSource(Model* model, std::string &src)
     std::string line;
     while(std::getline(srcStream, line))
     {
-        ;
+        size_t var = line.find("class");
+        size_t varEnd;
+        if(var != std::string::npos)
+        {
+            var = line.find(" ", var) + 1;
+            varEnd = line.find(" ", var) - var;
+            model->modelClass = line.substr(var, varEnd);
+        }
+        var = line.find("ModelRenderer ");
+        if(var != std::string::npos)
+        {
+            var = line.find(" ", var) + 1;
+            varEnd = line.find(";", var) - var;
+            std::string partName = line.substr(var, varEnd);
+//            std::cout << partName;
+            model->addPart(partName);
+        }
+        for(auto& part : model->parts)
+        {
+            if(line.find(part.first))
+            {
+                var = line.find("new ModelRenderer(");
+                if(var != std::string::npos)
+                {
+                    var = line.find(", ", var) + 2;
+                    varEnd = line.find(", ", var) - var;
+                    std::string partName = line.substr(var, varEnd);
+                    part.second->textureOffsetX = atoi(partName.c_str());
+                    var = line.find(", ", var) + 2;
+                    varEnd = line.find(")") - var;
+                    partName = line.substr(var, varEnd);
+                    part.second->textureOffsetY = atoi(partName.c_str());
+                }
+                //Wow this isn't working right at all
+                var = line.find(part.first + ".addBox", var + 1);
+                if(var != std::string::npos)
+                {
+                    var = line.find("(", var) + 2;
+                    float nums[6];
+                    for(int i = 0; i < 6; i++)
+                    {
+                        varEnd = endOfNum(line, var) - var;
+                        std::string value = line.substr(var, varEnd).c_str();
+                        std::cout << value << " | " << var << " | " << varEnd << "\n";
+                        nums[i] = atof(value.c_str());
+                        var = line.find(", ", var) + 2;
+                    }
+                    part.second->addBox(nums[0], nums[1], nums[2], nums[3], nums[4], nums[5]);
+                }
+                if(var != std::string::npos)
+                {
+                    var = line.find("(", var) + 2;
+                    float nums[3];
+                    for(int i = 0; i < 3; i++)
+                    {
+                        varEnd = endOfNum(line, var) - var;
+                        std::string value = line.substr(var, varEnd).c_str();
+                        std::cout << value << " | " << var << " | " << varEnd << "\n";
+                        nums[i] = atof(value.c_str());
+                        var = line.find(", ", var) + 2;
+                    }
+                    part.second->setRotationPoint(nums[0], nums[1], nums[2]);
+                }
+            }
+        }
     }
 
 }
